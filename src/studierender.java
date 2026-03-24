044ce5f7916b9e7131e6d179364f6c47573956d8
tbekesch <tab6001@thi.de>
2026-03-24 11:36:43 +0100

    student angepasst

--- src/studierender.java
+++ src/studierender.java
@@ -3,13 +3,13 @@ public class studierender {
     private String matrikelnummer;
     private String studiengang;
 
-    public studierender (name, matrikelnummer, studiengang) {
+    public studierender (String name, String matrikelnummer, String studiengang) {
         this.name = name;
         this.matrikelnummer = matrikelnummer;
         this.studiengang = studiengang;
     }
 
-    public String frageStellen (dozierender) {
-        return String "Meine Frage";
+    public String frageStellen (dozierender dozierender) {
+        return "Meine Frage";
     }
 }
