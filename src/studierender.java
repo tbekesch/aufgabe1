a359879d2e54801eaaf7a63bfa08a2e1162ae419
tbekesch <tab6001@thi.de>
2026-03-24 10:31:02 +0100

    student klasse ergaenzt

--- src/studierender.java
+++ src/studierender.java
@@ -2,4 +2,14 @@ public class studierender {
     private String name;
     private String matrikelnummer;
     private String studiengang;
+
+    public studierender (name, matrikelnummer, studiengang) {
+        this.name = name;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+
+    public String frageStellen (dozierender) {
+        return String "Meine Frage";
+    }
 }
